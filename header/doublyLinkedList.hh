a93cb7f5cf09a598a7ac55a6b19f1a704bc89ac5
sergio <sergioleesp@gmail.com>
2022-10-02 20:54:48 +0200

    Defined several methods

--- header/doublyLinkedList.hh
+++ header/doublyLinkedList.hh
@@ -57,66 +57,363 @@ namespace ds_cpp
     template <class T>
     class doublyLinkedList
     {
-        private:
-            struct node
-            {
-                T val;
-                node *next;
-                node *prev;
-            };
-            node *head;
-            node *tail;
-            int sizeOfList;
+    private:
+        struct node
+        {
+            T val;
+            node *next;
+            node *prev;
+        };
+        node *head;
+        node *tail;
+        int sizeOfList;
 
-        public:
-            doublyLinkedList()
+    public:
+        doublyLinkedList()
+        {
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->sizeOfList = 0;
+        }
+
+        doublyLinkedList(const doublyLinkedList<T> &list)
+        {
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->sizeOfList = 0;
+            node *curr = list.head;
+            while (curr != nullptr)
+            {
+                this->push_back(curr->val);
+                curr = curr->next;
+            }
+        }
+
+        doublyLinkedList(int sizeOfList)
+        {
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->sizeOfList = 0;
+            for (int i = 0; i < sizeOfList; i++)
+            {
+                T garbageValue;
+                this->push_back(garbageValue);
+            }
+        }
+
+        doublyLinkedList(int sizeOfList, const T &val)
+        {
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->sizeOfList = 0;
+            for (int i = 0; i < sizeOfList; i++)
+            {
+                this->push_back(val);
+            }
+        }
+
+        void push_back(const T &val)
+        {
+            node *newNode = new node;
+            newNode->val = val;
+            newNode->next = nullptr;
+            if (this->head == nullptr)
+            {
+                this->head = newNode;
+                this->tail = newNode;
+                newNode->prev = nullptr;
+            }
+            else
+            {
+                this->tail->next = newNode;
+                newNode->prev = this->tail;
+                this->tail = newNode;
+            }
+            this->sizeOfList++;
+            return;
+        }
+
+        void push_front(const T &val)
+        {   
+            node *newNode = new node;
+            newNode->val = val;
+            newNode->prev = nullptr;
+            if (!this->head)
+            {
+                this->head = newNode;
+                this->tail = newNode;
+                newNode->next = nullptr;
+            }
+            else
+            {
+                newNode->next = this->head;
+                this->head->prev = newNode;
+                this->head = newNode;
+            }
+            this->sizeOfList++;
+            return;
+        }
+
+        T pop_back()
+        {
+            if (this->tail == nullptr)
+            {
+                throw std::runtime_error("cannot pop from an empty list");
+            }
+            T val = this->tail->val;
+            if (this->head == this->tail)
             {
                 this->head = nullptr;
                 this->tail = nullptr;
-                this->sizeOfList = 0;
+            }
+            else
+            {   
+                node *curr = this->tail;
+                this->tail = this->tail->prev;
+                this->tail->next = nullptr;
+                delete curr;
             }
 
-            doublyLinkedList(const doublyLinkedList<T> &list)
+            this->sizeOfList--;
+            return val;
+        }
+
+        T pop_front()
+        {
+            if (this->head == nullptr)
             {
-                this->head = nullptr;
+                throw std::runtime_error("cannot pop from an empty list");
+            }
+            T val = this->head->val;
+            node *curr = this->head;
+            this->head = this->head->next;
+            if (this->head == nullptr)
+            {
                 this->tail = nullptr;
-                this->sizeOfList = 0;
-                node *curr = list.head;
-                while (curr != nullptr)
+            }
+            else
+            {
+                this->head->prev = nullptr;
+            }
+            delete curr;
+            this->sizeOfList--;
+            return val;
+        }
+
+        T &back()
+        {
+            if (this->tail == nullptr)
+            {
+                throw std::runtime_error("list is empty");
+            }
+            return this->tail->val;
+        }
+
+        const T &back() const
+        {
+            if (this->tail == nullptr)
+            {
+                throw std::runtime_error("list is empty");
+            }
+            return this->tail->val;
+        }
+
+        T &front()
+        {
+            if (this->head == nullptr)
+            {
+                throw std::runtime_error("list is empty");
+            }
+            return this->head->val;
+        }
+
+        const T &front() const
+        {
+            if (this->head == nullptr)
+            {
+                throw std::runtime_error("list is empty");
+            }
+            return this->head->val;
+        }
+
+        void insert(const T &val)
+        {
+            this->push_back(val);
+            return;
+        }
+
+        void insert(const T &val, int index)
+        {
+            if (index < 0 || index > this->size())
+            {
+                throw std::runtime_error("list index out of bounds");
+            }
+            if (index == 0)
+            {
+                this->push_front(val);
+            }
+            else if (index == this->size())
+            {
+                this->push_back(val);
+            }
+            else
+            {   
+                node *newNode = new node;
+                newNode->val = val;
+                node *curr = this->head;
+                for (int i = 0; i < index - 1; i++)
                 {
-                    this->push_back(curr->val);
                     curr = curr->next;
                 }
+                newNode->next = curr->next;
+                newNode->prev = curr;
+                curr->next->prev = newNode;
+                curr->next = newNode;
+                this->sizeOfList++;
             }
+            return;
+        }
 
-            doublyLinkedList(int sizeOfList)
+        void erase()
+        {
+            node *curr = this->head;
+            while (curr != nullptr) 
             {
-                this->head = nullptr;
-                this->tail = nullptr;
-                this->sizeOfList = 0;
-                for (int i = 0; i < sizeOfList; i++)
+                node *temp = curr->next;
+                delete curr;
+                curr = temp;
+            }
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->sizeOfList = 0;
+            return;
+        }
+
+        void erase(int index)
+        {
+            if (index < 0 || index >= this->size())
+            {
+                throw std::runtime_error("list index out of bounds");
+            }
+            if (index == 0)
+            {
+                node *temp = this->head->next;
+                delete this->head;
+                this->head = temp;
+                if (this->head == nullptr)
                 {
-                    T garbageValue;
-                    this->push_back(garbageValue);
+                    this->tail = nullptr;
+                }
+                else
+                {
+                    temp->prev = nullptr;
                 }
             }
-
-            doublyLinkedList(int sizeOfList, const T &val)
+            else
             {
-                this->head = nullptr;
-                this->tail = nullptr;
-                this->sizeOfList = 0;
-                for (int i = 0; i < sizeOfList; i++)
+                node *curr = this->head;
+                for (int i = 0; i < index - 1; i++)
                 {
-                    this->push_back(val);
+                    curr = curr->next;
                 }
-            }
-
-            void push_back(const T &val)
-            {
+                node *temp = curr->next->next;
+                delete curr->next;
+                curr->next = temp;
+                if (curr->next == nullptr)
+                {
+                    this->tail = curr;
+                }
+                else
+                {
+                    temp->prev = curr;
+                }
                 
             }
+            this->sizeOfList--;
+            return;
+        }
 
+        void erase(int start, int end)
+        {
+            if (start < 0 || start >= this->size() || end < 0 || end >= this->size())
+            {
+                throw std::runtime_error("list index out of bounds");
+            }
+            if (start > end)
+            {
+                throw std::runtime_error("starting index is greater than ending index");
+            }
+            node *nodeOneBeforeStart = nullptr;
+            node *curr = this->head;
+            for (int i = 0; i < start; i++)
+            {
+                nodeOneBeforeStart = curr;
+                curr = curr->next;
+            }
+            node *nodeOneAfterEnd = curr;
+            for (int i = start; i <= end; i++)
+            {
+                nodeOneAfterEnd = nodeOneAfterEnd->next;
+            }
+            if (nodeOneBeforeStart == nullptr)
+            {
+                curr = this->head;
+                while (curr != nodeOneAfterEnd)
+                {
+                    node *temp = curr->next;
+                    delete curr;
+                    curr = temp;
+                }
+                this->head = nodeOneAfterEnd;
+                if (this->head == nullptr)
+                {
+                    this->tail = nullptr;
+                }
+            }
+            else
+            {
+                curr = nodeOneBeforeStart->next;
+                while (curr != nodeOneAfterEnd)
+                {
+                    node *temp = curr->next;
+                    delete curr;
+                    curr = temp;
+                }
+                nodeOneBeforeStart->next = nodeOneAfterEnd;
+                if (nodeOneAfterEnd == nullptr)
+                {
+                    this->tail = nodeOneBeforeStart;
+                }
+            }
+            this->sizeOfList -= (end - start + 1);
+            return;
+        }
+
+        void clear()
+        {
+            this->erase();
+            return;
+        }
+
+        void reverse()
+        {
+
+        }
+
+        int size() const
+        {
+            return this->sizeOfList;
+        }
+
+        bool empty()
+        {
+            return this->sizeOfList == 0;
+        }
+
+        ~doublyLinkedList()
+        {
+            this->clear();
+        }
     };
 }
 
